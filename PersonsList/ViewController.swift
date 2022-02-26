55ce4c4efdcc4ec714088db41116be798b82cff2
soloexploder <soloexploder@gmail.com>
2022-02-26 16:21:52 +0300

    Initial Commit

---
+++ PersonsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonsList
+//
+//  Created by Даниил Хантуров on 26.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
